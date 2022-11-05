ef77ef7123ae180e8d6c7f9d1aaa2885fe6145d4
Vasiliy_Nitro <v_shibayev@mail.ru>
2022-11-05 15:44:28 +0600

    Добавлены функции вставки и обхода по дереву

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-class TreeNode:
+class Node:
     """Класс Узел дерева"""
     def __init__(self, value):
         self.value = value
+        self.left = None
         self.right = None
 
 
@@ -9,19 +10,72 @@ class Tree:
     """Класс Дерево"""
     def __init__(self):
         self.root = None
+        self.tree_to_list = []
 
-    def insert_item(self, value):
+    def inorder(self) -> list:
+        """Возвращает упорядоченный список как результат обхода дерева"""
+        self.tree_to_list = []
+        return self.__inorder__(self.root)
+
+    def __inorder__(self, root: Node) -> list:
+        """Возвращает упорядоченный список как результат обхода дерева
+        (рекурсивная функция)"""
+        if root is not None:
+
+            # Обход левого поддерева
+            self.__inorder__(root.left);
+
+            # Корень (под) дерева
+            self.tree_to_list.append(root.value)
+
+            # Обход правого поддерева
+            self.__inorder__(root.right)
+
+            return self.tree_to_list
+
+
+    def insert(self, value) -> None:
         """Вставка узла дерева"""
 
         # Новый узел
-        new_item = TreeNode(value)
+        new_item = Node(value)
 
         # Если дерево не существует, то оно создается (создается корень)
         if not self.root:
             self.root = new_item
         else:
-            pass
-        
-    def __insert__(self, root, node):
-        pass
+            self.__insert_item__(self.root, new_item)
 
+    def __insert_item__(self, node: Node, new_node: Node) -> None:
+        """Вставка нового узла (рекурсивная функция)"""
+
+        # Если значение уже существует, выбрасывается ошибка
+        if new_node.value == node.value:
+            raise ValueError(f"Значение < {new_node.value} > существует.")
+
+        if new_node.value < node.value:
+            # Вставка по левой ветке
+            if not node.left:
+                node.left = new_node
+                return
+            else:
+                self.__insert_item__(node.left, new_node)
+        else:
+            # Вставка по правой ветке
+            if not node.right:
+                node.right = new_node
+                return
+            else:
+                self.__insert_item__(node.right, new_node)
+
+
+tree = Tree()
+tree.insert(8)
+tree.insert(7)
+tree.insert(3)
+tree.insert(10)
+tree.insert(14)
+tree.insert(6)
+tree.insert(1)
+tree.insert(4)
+print(tree.inorder())
